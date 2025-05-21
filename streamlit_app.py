3989438ec74734e61d414f6667fad62ca71c27b0
rapkii <muhammadrafki250@gmail.com>
2025-05-21 09:31:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🤠rapkiiareaaaa")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    " selamat datang, disini area untuk bersenang-senang "

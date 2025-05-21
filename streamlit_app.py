2fd0a50569846c98c430249e3706da95c5634253
rapkii <muhammadrafki250@gmail.com>
2025-05-21 15:42:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("🤠rapkiiareaaaa")
 st.write("selamat datang, disini area untuk bersenang-senang") 
-    
+(    
 st.image("IMG_20250520_161840_349.jpg", width= 200) 
     
